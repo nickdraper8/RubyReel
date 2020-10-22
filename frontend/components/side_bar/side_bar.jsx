136e85df640966a5f23ee6263e24a12520e34b0f
nickdraper <nickdraper8@gmail.com>
2020-10-22 16:08:34 -0400

    added footer to sidebar, also fixed modal sidebar positioning

--- frontend/components/side_bar/side_bar.jsx
+++ frontend/components/side_bar/side_bar.jsx
@@ -38,6 +38,25 @@ class SideBar extends React.Component {
                         <a href="https://www.linkedin.com/in/nicholas-draper/"><i className="fab fa-linkedin"></i>LinkedIn</a>
                         <a href="https://github.com/nickdraper8/RubyReel"><i className="fas fa-gem"></i>Project Repo</a>
                     </div>
+                    <div id="sidebar-footer">
+                        <div id="sidebar-footer-section1">
+                            <a href="https://github.com/nickdraper8/RubyReel">About</a>
+                            <a href="https://github.com/nickdraper8/RubyReel">Press</a>
+                            <a href="https://github.com/nickdraper8/RubyReel">Copyright</a>
+                            <a href="https://github.com/nickdraper8/RubyReel">Contact me</a>
+                            <a href="https://github.com/nickdraper8/RubyReel">Creator</a>
+                            <a href="https://github.com/nickdraper8/RubyReel">Advertise</a>
+                            <a href="https://github.com/nickdraper8/RubyReel">Developers</a>
+                        </div>
+                        <div id="sidebar-footer-section2">
+                            <a href="https://github.com/nickdraper8/RubyReel">Terms</a>
+                            <a href="https://github.com/nickdraper8/RubyReel">Privacy</a>
+                            <a href="https://github.com/nickdraper8/RubyReel">Policy & Safety</a>
+                            <a href="https://github.com/nickdraper8/RubyReel">How RubyReel works</a>
+                            <a href="https://github.com/nickdraper8/RubyReel">Test new features</a>
+                        </div>
+                        <div id="sidebar-footer-copyright">© 2020 Nick Draper</div>
+                    </div>
                 </section>
             );
         } else {
@@ -58,6 +77,25 @@ class SideBar extends React.Component {
                         <a href="https://angel.co/u/nicholas-draper-2"><i className="fab fa-angellist"></i>AngelList</a>
                         <a href="https://www.linkedin.com/in/nicholas-draper/"><i className="fab fa-linkedin"></i>LinkedIn</a>
                         <a href="https://github.com/nickdraper8/RubyReel"><i className="fas fa-gem"></i>Project Repo</a>
+                    </div>
+                    <div id="sidebar-footer">
+                        <div id="sidebar-footer-section1">
+                            <a href="https://github.com/nickdraper8/RubyReel">About</a>
+                            <a href="https://github.com/nickdraper8/RubyReel">Press</a>
+                            <a href="https://github.com/nickdraper8/RubyReel">Copyright</a>
+                            <a href="https://github.com/nickdraper8/RubyReel">Contact me</a>
+                            <a href="https://github.com/nickdraper8/RubyReel">Creator</a>
+                            <a href="https://github.com/nickdraper8/RubyReel">Advertise</a>
+                            <a href="https://github.com/nickdraper8/RubyReel">Developers</a>
+                        </div>
+                        <div id="sidebar-footer-section2">
+                            <a href="https://github.com/nickdraper8/RubyReel">Terms</a>
+                            <a href="https://github.com/nickdraper8/RubyReel">Privacy</a>
+                            <a href="https://github.com/nickdraper8/RubyReel">Policy & Safety</a>
+                            <a href="https://github.com/nickdraper8/RubyReel">How RubyReel works</a>
+                            <a href="https://github.com/nickdraper8/RubyReel">Test new features</a>
+                        </div>
+                        <div id="sidebar-footer-copyright">© 2020 Nick Draper</div>
                     </div>
                 </section>
             );
